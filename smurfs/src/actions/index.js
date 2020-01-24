77bbbab6b4bb3bb0c5a1d25831995a25df2c74be
unknown <viola4lfe@gmail.com>
2020-01-24 12:34:34 -0500

    wrote first action for fetching smurf

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -9,3 +9,17 @@ export const FETCH_SMURF_FAIL = "FETCH_SMURF_FAIL";
 export const POST_SMURF_START = "POST_SMURF_START";
 export const POST_SMURF_SUCCESS = "POST_SMURF_SUCCESS";
 export const POST_SMURF_FAIL = "POST_SMURF_FAIL";
+
+export const fetchSmurf = () => dispatch => {
+  dispatch({ type: FETCH_SMURF_START });
+  axios
+    .get(``)
+    .then(res => {
+      // console.log(res);
+      dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      // console.log(err);
+      dispatch({ type: FETCH_SMURF_FAIL, payload: err });
+    });
+};
