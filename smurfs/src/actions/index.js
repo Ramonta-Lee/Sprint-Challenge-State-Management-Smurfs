7aeed1d767323459d4e4c524a1d2490909b5d249
unknown <viola4lfe@gmail.com>
2020-01-24 12:01:04 -0500

    set up initialState for Brainey and imported named exports from reducers index.js file

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+// For fetching data
+export const FETCH_SMURF_START = "FETCH_SMURF_START";
+export const FETCH_SMURF_SUCCESS = "FETCH_SMURF_SUCCESS";
+export const FETCH_SMURF_FAIL = "FETCH_SMURF_FAIL";
+
+// for posting data
+export const POST_SMURF_START = "POST_SMURF_START";
+export const POST_SMURF_SUCCESS = "POST_SMURF_SUCCESS";
+export const POST_SMURF_FAIL = "POST_SMURF_FAIL";
---
+++ smurfs/src/reducers/fetchSmurfReducer.js
@@ -0,0 +1,14 @@
+import {
+  Fetch_SMURF_START,
+  FETCH_SMURF_SUCCESS,
+  FETCH_SMURF_FAIL
+} from "../actions";
+
+const initialState = [
+  {
+    name: "Brainey",
+    age: 200,
+    height: "5cm",
+    id: 0
+  }
+];
